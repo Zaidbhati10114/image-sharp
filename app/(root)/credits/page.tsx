5e37fe726c90fff2516cf2c5c7c6c3b5cd41ea32
Zaidbhati10114 <zaidbhati007@gmail.com>
2024-02-21 15:09:14 +0530

    Initial Commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreditsPage = () => {
+  return <div>Creditspage</div>;
+};
+
+export default CreditsPage;
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransformationsPage = () => {
+  return <div>Transformationspage</div>;
+};
+
+export default TransformationsPage;
